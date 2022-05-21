e294c8d07bcae76c709d2c38cdec448ea9cbf2a9
sagar-pardhi <sgrpardhi007@gmail.com>
2022-05-21 20:35:35 +0530

    added models

---
+++ shortener_app/models.py
@@ -0,0 +1,15 @@
+from email.policy import default
+from enum import unique
+from sqlalchemy import Boolean, Column, Integer, String
+
+from .database import Base
+
+class URL(Base):
+    __tablename__ = "urls"
+
+    id = Column(Integer, primary_key=True)
+    key = Column(String, unique=True, index=True)
+    secret_key = Column(String, unique=True, index=True)
+    target_url = Column(String, index=True)
+    is_active = Column(Boolean, default=True)
+    click = Column(Integer, default=0)
