94f7e4ff5dc80e510fe55dd9ea62a7d4e3b47ddf
sagar-pardhi <sgrpardhi007@gmail.com>
2022-05-23 20:11:19 +0530

    added db connectivity

--- shortener_app/main.py
+++ shortener_app/main.py
@@ -1,9 +1,22 @@
-import validators
-from fastapi import FastAPI, HTTPException
+import secrets
 
-from . import schemas
+import validators
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
+
+from . import models, schemas
+from .database import SessionLocal, engine
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 app = FastAPI()
+models.Base.metadata.create_all(bind=engine)
 
 def raise_bad_request(message):
     raise HTTPException(status_code=400, detail=message)
@@ -14,8 +27,21 @@ def read_root():
     return "Welcome to URL shortener API :)"
 
 
-@app.post("/url")
-def create_url(url: schemas.URLBase):
+@app.post("/url", response_model=schemas.URLInfo)
+def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
     if not validators.url(url.target_url):
         raise_bad_request(message="Your provided url is not valid")
-    return f"TODO: create database entry for: {url.target_url}"
+    
+    chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    key = "".join(secrets.choice(chars) for _ in range(5))
+    secret_key = "".join(secrets.choice(chars) for _ in range(8))
+    db_url = models.URL(
+        target_url = url.target_url, key=key, secret_key=secret_key
+    )
+    db.add(db_url)
+    db.commit()
+    db.refresh(db_url)
+    db_url.url = key
+    db_url.admin_url = secret_key
+
+    return db_url
