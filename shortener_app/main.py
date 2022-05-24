76bba39fd8a1e7dd0b80c62120dc860dfc4f9274
sagar-pardhi <sgrpardhi007@gmail.com>
2022-05-24 20:26:13 +0530

    removed crud logic

--- shortener_app/main.py
+++ shortener_app/main.py
@@ -1,10 +1,11 @@
-import secrets
 from urllib.request import Request
 
 import validators
 from fastapi import Depends, FastAPI, HTTPException, Request
 from fastapi.responses import RedirectResponse
 from sqlalchemy.orm import Session
+
+from shortener_app import crud
 
 from . import models, schemas
 from .database import SessionLocal, engine
@@ -41,15 +42,9 @@ def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
     if not validators.url(url.target_url):
         raise_bad_request(message="Your provided url is not valid")
 
-    chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    key = "".join(secrets.choice(chars) for _ in range(5))
-    secret_key = "".join(secrets.choice(chars) for _ in range(8))
-    db_url = models.URL(target_url=url.target_url, key=key, secret_key=secret_key)
-    db.add(db_url)
-    db.commit()
-    db.refresh(db_url)
-    db_url.url = key
-    db_url.admin_url = secret_key
+    db_url = crud.create_db_url(db=db, url=url)
+    db_url.url = db_url.key
+    db_url.admin_url = db_url.secret_key
 
     return db_url
 
@@ -63,7 +58,7 @@ def forward_to_target_url(
         .filter(models.URL.key == url_key, models.URL.is_active)
         .first()
     )
-    if db_url:
+    if db_url := crud.get_db_url_by_key(db=db, url_key=url_key):
         return RedirectResponse(db_url.target_url)
     else:
         raise_not_found(request)
