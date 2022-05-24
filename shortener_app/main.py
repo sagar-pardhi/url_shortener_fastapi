e11ac9197879c2c35c43383d0fa069739d104cd8
sagar-pardhi <sgrpardhi007@gmail.com>
2022-05-24 19:59:03 +0530

    added endpoint to forward to target url

--- shortener_app/main.py
+++ shortener_app/main.py
@@ -1,7 +1,9 @@
 import secrets
+from urllib.request import Request
 
 import validators
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException, Request
+from fastapi.responses import RedirectResponse
 from sqlalchemy.orm import Session
 
 from . import models, schemas
@@ -15,10 +17,17 @@ def get_db():
     finally:
         db.close()
 
+
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
 
+
 def raise_bad_request(message):
+    raise HTTPException(status_code=400, detail=message)
+
+
+def raise_not_found(request):
+    message = f"URL '{request.url}' doesn't exist"
     raise HTTPException(status_code=400, detail=message)
 
 
@@ -31,13 +40,11 @@ def read_root():
 def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
     if not validators.url(url.target_url):
         raise_bad_request(message="Your provided url is not valid")
-    
+
     chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     key = "".join(secrets.choice(chars) for _ in range(5))
     secret_key = "".join(secrets.choice(chars) for _ in range(8))
-    db_url = models.URL(
-        target_url = url.target_url, key=key, secret_key=secret_key
-    )
+    db_url = models.URL(target_url=url.target_url, key=key, secret_key=secret_key)
     db.add(db_url)
     db.commit()
     db.refresh(db_url)
@@ -45,3 +52,18 @@ def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
     db_url.admin_url = secret_key
 
     return db_url
+
+
+@app.get("/{url_key}")
+def forward_to_target_url(
+    url_key: str, request: Request, db: Session = Depends(get_db)
+):
+    db_url = (
+        db.query(models.URL)
+        .filter(models.URL.key == url_key, models.URL.is_active)
+        .first()
+    )
+    if db_url:
+        return RedirectResponse(db_url.target_url)
+    else:
+        raise_not_found(request)
